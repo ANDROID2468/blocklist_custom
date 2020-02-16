6bc07a6144c5fd72e0dcfe02c2ea44f3f128ed38
ANDROID2468 <39718118+ANDROID2468@users.noreply.github.com>
2020-02-15 23:17:47 -0600

    updated init.sh

--- init.sh
+++ init.sh
@@ -11,7 +11,19 @@ then
 fi
 
 
-cp -r blocklist_custom/* hosts/
+cp -r blocklist_custom/server.sh hosts/
 cd ~/hosts
-rm -f init.sh
-echo done!
+echo "making server.sh executable..."
+chmod +x server.sh
+
+read -p "do you want to delete the blocklist folder?(not needed anymore after running this command) [y,n] " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "The blocklist folder will Self-destruct in 0 sec!"
+    rm -rf ~/blocklist_costom/
+    echo "BOOM!"
+    echo "..."
+fi
+
+echo "done!"
