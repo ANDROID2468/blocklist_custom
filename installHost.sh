8302d71459d3030694ceede244c6a312f338c91c
ANDROID2468 <39718118+ANDROID2468@users.noreply.github.com>
2020-02-17 20:42:52 -0600

    Create installHost.sh

---
+++ installHost.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#By ANDROID2468
+
+rm -f blacklist
+echo "Downloading costom blacklist file..."
+curl -O https://raw.githubusercontent.com/ANDROID2468/blocklist_custom/master/blacklist
+echo "now running hosts updater"
+rm -f out/hosts
+python updateHostsFile.py
+echo "copying host file to out folder"
