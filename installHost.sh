c999678eeb3bab6dbbbb27cff990a2e575d9d850
ANDROID2468 <39718118+ANDROID2468@users.noreply.github.com>
2020-02-17 20:45:34 -0600

    Delete installHost.sh

--- installHost.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#By ANDROID2468
-
-rm -f blacklist
-echo "Downloading costom blacklist file..."
-curl -O https://raw.githubusercontent.com/ANDROID2468/blocklist_custom/master/blacklist
-echo "now running hosts updater"
-python updateHostsFile.py
-echo "Done!"
